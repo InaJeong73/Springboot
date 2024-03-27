cd17baa3f15ec545fcb343f2cf92a15f9259ddc0
InaJeong73 <shlovejo2@ajou.ac.kr>
2024-03-27 23:56:14 +0900

    first commit

---
+++ ch1/src/test/java/com/fastcampus/ch1/Ch1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fastcampus.ch1;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
