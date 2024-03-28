a54750a62f64f6ecea849d9a9baafb37e739ae3f
InaJeong73 <shlovejo2@ajou.ac.kr>
2024-03-28 23:43:32 +0900

    2nd commit

--- ch1/src/main/java/com/fastcampus/ch1/Ch1Application.java
+++ ch1/src/main/java/com/fastcampus/ch1/Ch1Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController//이 프로그램은 웹에서 호출할 수 있는 프로그램이라고 등록
+//controller로 등록된 프로그램은 spring 이 자동으로 static으로 변환시키기 때문에 따로 객체 생성을 하지 않아도 생성된다.
 @SpringBootApplication
 public class Ch1Application {
 
