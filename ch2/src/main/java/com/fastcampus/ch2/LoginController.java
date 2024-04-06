b0631be55ecaed634fcb3d58d5f96127d15b7953
InaJeong73 <shlovejo2@ajou.ac.kr>
2024-04-06 23:53:25 +0900

    7th commit

--- ch2/src/main/java/com/fastcampus/ch2/LoginController.java
+++ ch2/src/main/java/com/fastcampus/ch2/LoginController.java
@@ -1,8 +1,10 @@
 package com.fastcampus.ch2;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.net.URLEncoder;
 
@@ -22,7 +24,7 @@ public class LoginController {
     //@RequestMapping(value="/login/login", method= RequestMethod.POST)
     //@PostMapping("/login/login")
     @PostMapping("/login")
-    public String login(String id, String pwd, Model model)throws Exception{
+    public String login(Model model,String id, String pwd, RedirectAttributes redirectAttribites)throws Exception{
         //1. id, pwd를 확인
         if(loginCheck(id,pwd)){
             //2. 일치하면, userInfo.html
@@ -31,11 +33,18 @@ public class LoginController {
             return "userInfo";
         }else{
             //3. 일치하지 않으면, login.html
-            String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.");
-            return "redirect:/login/login?msg="+msg;//redirect의 경우 GET 요청이다.
+            //String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.");
+            String msg = "id 또는 pwd가 일치하지 않습니다";
+            redirectAttribites.addAttribute("msg", msg);
+            redirectAttribites.addFlashAttribute("msg", "일회용 메세지");
+            redirectAttribites.addFlashAttribute("msg","request 에 저장된 msg");
+            //setAttribute는 저장, getAttribute는 데이터를 가져오는것.
+            return "redirect:/";
+            //redirect의 경우 GET 요청이다.
+            //return "redirect:/login/login?msg="+msg;
+            //redirect의 경우 GET 요청이다.
+
         }
-
-
     }
 
     private boolean loginCheck(String id, String pwd) {
