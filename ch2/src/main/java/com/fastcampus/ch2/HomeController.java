695715d65048563ccd708462097f57ee67055860
InaJeong73 <shlovejo2@ajou.ac.kr>
2024-04-07 23:57:02 +0900

    8th commit

--- ch2/src/main/java/com/fastcampus/ch2/HomeController.java
+++ ch2/src/main/java/com/fastcampus/ch2/HomeController.java
@@ -1,13 +1,24 @@
 package com.fastcampus.ch2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String main(){
-        return "index";
+        return "index";//templates/index.html
+    }
+    @GetMapping("/test")
+    public String test(Model model){
+        model.addAttribute("lastName", "Ina");
+        model.addAttribute("firstName", "Jeong");
+        model.addAttribute("list", Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee"));
+        return "test";//templates/test.html
     }
 
 }
